f08d0d179c8660ae0351ac6b313001b7de3db3a9
rob467 <133133177+rob467@users.noreply.github.com>
2025-01-28 20:07:47 +0100

    Created functions for adding items to form

--- src/todo-form.js
+++ src/todo-form.js
@@ -1,20 +1,52 @@
 import { TodoItem, Project } from "./todoItems.js"
 
-function createHtmlEl(tag="div", parent, props={}, textContent="") {
+function createHtmlEl({tag="div", parent, props={}, textContent=""}) {
     let element = document.createElement(tag);
     Object.assign(element, props);
-    parent.append(element);
     element.textContent = textContent;
+    parent.append(element);
     return element;
 }
 
-const getText = document.createTextNode.bind(document);
+function createHtmlLabelInput({parent, forName,
+    inputType="text", labelTextContent="", required=false} = {}) {
+    const label = document.createElement("label");
+    const input = document.createElement("input");
+
+    label.htmlFor = forName;
+    label.textContent = labelTextContent;
+
+    input.setAttribute("id", forName);
+    input.setAttribute("name", forName);
+    input.setAttribute("type", inputType);
+    input.setAttribute("required", required);
+
+    parent.appendChild(label, input)
+    parent.appendChild(input)
+}
+
 
 function createTodoItemForm() {
     const heroDiv = document.querySelector(".hero")
-    const formDiv = createHtmlEl("div", heroDiv, {className: "form-container"});
-    const formToDo = createHtmlEl("form", formDiv, {className: "todo-form"},)
-    const labelTitle = createHtmlEl("label", formDiv, {}, "")
+    const formDiv = createHtmlEl({tag: "div", parent: heroDiv, props: {className: "form-container"}});
+    const formToDo = createHtmlEl({tag: "form", parent: formDiv, props: {className: "todo-form"},});
+
+    createHtmlLabelInput({
+        parent: formToDo,
+        forName: "title",
+        labelTextContent: "Title: ",
+        required: true});
+  
+        createHtmlLabelInput({
+            parent: formToDo,
+            forName: "due-date",
+            labelTextContent: "Due Date: ",
+            inputType: "date",
+            required: true});
+
+    
+    // const priorityLabel = createHtmlLabelInput("label", formDiv, "Low-Priority");
+    // const priorityInput = createHtmlLabelInput("input", formDiv, {name: "low-priority", id: "low-priority", type: "checkbox"});
 }
 
 export default createTodoItemForm
